27bec4dd8e560e592c184c3310e841470709b7da
Jesus <jesus.fy.1997@gmail.com>
2020-02-18 03:46:24 -0300

    Models User and note

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+UserSchema.methods.encrypPassword = async password => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+UserSchema.methods.matchPassword = function( password ){
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
